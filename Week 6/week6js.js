0ea734d946181668278ff848228c35c9b62f7695
matthewfranciaramos <matthewaurumramos@gmail.com>
2024-05-30 12:32:08 +0800

    made activity 5 and week6

---
+++ Week 6/week6js.js
@@ -0,0 +1,29 @@
+let username =[];
+let forms = document.getElementById("Data");
+let getUsername = document.getElementById("UN");
+let validateButton = document.getElementById("Validation");
+let checkUsername;
+
+validateButton.addEventListener("click", event =>{
+   let username = getUsername.value;
+   
+   CheckValidation(username);
+})
+
+function CheckValidation(username) 
+{ 
+    let usernameSize = username;
+    let isUsernameValid = false;
+    
+    const CharCount=  /^[A0-Z0-z0]{9,}$/;
+    if(!CharCount.test(usernameSize)) 
+    { 
+    alert("Error, No Special Characters or minimum characters");
+    isUsernameValid = false;
+    }
+    else
+    { 
+    alert("Success");
+    return isUsernameValid = true;
+    }
+}
