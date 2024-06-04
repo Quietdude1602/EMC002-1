3b78d0291b2d8ac94d4060e384a2f0352e0ae5cf
matthewfranciaramos <matthewaurumramos@gmail.com>
2024-06-04 11:42:50 +0800

    Finished Activity 5

--- Week 6/week6js.js
+++ Week 6/week6js.js
@@ -1,38 +1,33 @@
-let username =[];
-let password =[];
-let forms = document.getElementById("Data");
-let getUsername = document.getElementById("UN");
-let getPassword = document.getElementById("PS");
-let validateButton = document.getElementById("Validation");
-let checkUsername;
-let checkPassword;
+// let username =[];
+// let forms = document.getElementById("Data");
+// let getUsername = document.getElementById("UN");
+// let validateButton = document.getElementById("Validation");
+// let checkUsername;
 
-validateButton.addEventListener("click", event =>{
-   let username = getUsername.value;
-   let password = getPassword.value;
+// validateButton.addEventListener("click", event =>{
+//    let username = getUsername.value;
 
-   CheckValidation(username);
-   SeeValidation(password);
-})
+//    CheckValidation(username);
+// })
 
-function CheckValidation(username) 
-{ 
-    let usernameSize = username;
-    let isUsernameValid = false;
+// function CheckValidation(username) 
+// { 
+//     let usernameSize = username;
+//     let isUsernameValid = false;
     
-    const CharCount=  /^[A0-Z0-z0]{7,}$/;
-    if(!CharCount.test(usernameSize)) 
-    { 
-    alert("Error, No Special Characters or minimum characters");
-    isUsernameValid = false;
-    }
-    else
-    { 
-    alert("Success");
-    return isUsernameValid = true;
-    }
-}
+//     const CharCount=  /^[a-z\d]+$/i;
+//     if(!CharCount.test(usernameSize)) 
+//     { 
+//     alert("Error, No Special Characters or minimum characters");
+//     isUsernameValid = false;
+//     }
+//     else
+//     { 
+//     alert("Success");
+//     return isUsernameValid = true;
+//     }
+// }
 
-function SeeValidation(password){
-    let
-}
+// function SeeValidation(password){
+//     let
+// }
